65c372d4109b9ed66738da9df585eef99e82c6f2
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-05 00:22:26 -0800

    Created Day 5 Project

---
+++ Day 5 - Seed Maps/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Advent of Code Day 5 - Seed Maps");
+
+//using var input = File.OpenText("..\\..\\..\\input.txt");
+using var input = File.OpenText("..\\..\\..\\sample.txt");
