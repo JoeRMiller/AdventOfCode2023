1ec4c398ad51e629bb68e801f121b4bc8062406f
JoeRMiller <JoeRMiller@gmail.com>
2023-12-05 21:31:59 -0800

    Added Race Struct

---
+++ Day 6 - Wait For It/Race.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCode2023
+{
+    public struct Race
+    {
+        public int Time { get; }
+        public int Distance { get; }
+
+        public Race(int time, int distance)
+        {
+            this.Time = time;
+            this.Distance = distance;
+        }
+    }
+}
