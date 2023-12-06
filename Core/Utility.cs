139d4be3e4d3fb5f93ca9bfc23a34c3a9ac4747d
JoeRMiller <JoeRMiller@gmail.com>
2023-12-05 21:28:35 -0800

    Created Day 6 project

---
+++ Core/Utility.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventofCode2023.Core
+{
+    public class Utility
+    {
+        public static List<string> ReadProjectFile(string path)
+        {
+            return File.ReadAllLines($"..\\..\\..\\{path}").ToList();
+        }
+    }
+}
---
+++ Day 6 - Wait For It/Program.cs
@@ -0,0 +1,18 @@
+﻿using AdventofCode2023.Core;
+using System.Diagnostics;
+
+Console.WriteLine("Day 6 - Wait For It");
+var input = Utility.ReadProjectFile("input.txt");
+
+//Part 1
+var watch = Stopwatch.StartNew();
+
+
+
+var result = 0;
+watch.Stop();
+Console.WriteLine($"Ways to win product: {result}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
+
+
+
