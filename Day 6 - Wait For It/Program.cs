e74529d19d24ce4c51de7951c58dcfb1b5a21bea
JoeRMiller <JoeRMiller@gmail.com>
2023-12-05 22:10:07 -0800

    Completed Task: 39570185

--- Day 6 - Wait For It/Program.cs
+++ Day 6 - Wait For It/Program.cs
@@ -15,7 +15,7 @@ foreach (Race race in races)
     var time = race.Time;
     var distance = race.Distance;
     var wins = 0;
-    for (int i = 1; i < time; i++)
+    for (long i = 1; i < time; i++)
     {
         var timeLeft = time - i;
         var travel = timeLeft * i;
@@ -39,6 +39,33 @@ var result = productTotal;
 watch.Stop();
 Console.WriteLine($"Ways to win product: {result}");
 Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
+
+//Part2
+watch.Start();
+Race combo = RaceFactory.CreateRace(input);
+
+var t = combo.Time;
+var d = combo.Distance;
+var w = 0;
+for (long i = 1; i < t; i++)
+{
+    var timeLeft = t - i;
+    var travel = timeLeft * i;
+    if (travel > d)
+    {
+        //This wait time beat the current best. 
+        w++;
+    }
+    if ((w > 0) && (travel < d))
+    {
+        //Losing distance, quit checking
+        break;
+    }
+}
+
+Console.WriteLine($"Ways to win: {w}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
+
 
 
 
--- Day 6 - Wait For It/Race.cs
+++ Day 6 - Wait For It/Race.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventofCode2023
 {
     public struct Race
     {
-        public int Time { get; }
-        public int Distance { get; }
+        public long Time { get; }
+        public long Distance { get; }
 
-        public Race(int time, int distance)
+        public Race(long time, long distance)
         {
             this.Time = time;
             this.Distance = distance;
@@ -29,10 +30,21 @@ namespace AdventofCode2023
             var count = line1.Count();
             for (int i = 1; i < count; i++)
             {
-                races.Add(new Race(int.Parse(line1[i]), int.Parse(line2[i])));
+                races.Add(new Race(long.Parse(line1[i]), long.Parse(line2[i])));
             }
 
             return races;
+        }
+
+        public static Race CreateRace(List<string> input) 
+        {
+            string[] items = input[0].Split(':', StringSplitOptions.TrimEntries);
+            string time = Regex.Replace(items[1], @"\s+", "");
+           
+            items = input[1].Split(':', StringSplitOptions.TrimEntries);
+            string distance = Regex.Replace(items[1], @"\s+", "");
+
+            return new Race(long.Parse(time), long.Parse(distance));
         }
     }
 }
