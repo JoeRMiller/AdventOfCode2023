8c14e4da78348838c4701b940300377dbfccab00
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-08 22:22:57 -0800

    Created Day 10 project template

---
+++ Day 10/Program.cs
@@ -0,0 +1,29 @@
+﻿using AdventofCode2023.Core;
+using AdventOfCode2023.Day10;
+using System.Diagnostics;
+
+Console.WriteLine("Advent of Code Day 10 - CHALLENGE NAME");
+
+//Part 1
+var watch = Stopwatch.StartNew();
+var input = Utility.ReadProjectFile("input.txt");
+//var input = Utility.ReadProjectFile("sample.txt");
+var result = 0;
+
+
+
+
+watch.Stop();
+Console.WriteLine($"Answer: {result}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
+
+
+//Part 2
+watch.Start();
+result = 0;
+
+
+
+watch.Stop();
+Console.WriteLine($"Answer: {result}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
