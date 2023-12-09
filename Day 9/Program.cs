a968ca0f3e9439cb68fddfe24e58b5830c576b98
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-08 21:01:10 -0800

    Initial Commit

---
+++ Day 9/Program.cs
@@ -0,0 +1,29 @@
+﻿using AdventofCode2023.Core;
+using System.Diagnostics;
+
+Console.WriteLine("Advent of Code Day 9 - Mirage Maintenance");
+
+//Part 1
+var watch = Stopwatch.StartNew();
+var input = Utility.ReadProjectFile("input.txt");
+//var input = Utility.ReadProjectFile("sample.txt");
+var result = 0;
+
+
+
+result = 0;
+watch.Stop();
+Console.WriteLine($"Answer: {result}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
+
+
+
+//Part 2
+watch.Start();
+
+
+
+result = 0;
+watch.Stop();
+Console.WriteLine($"Answer: {result}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
---
+++ Day 9/Readme.md
@@ -0,0 +1,28 @@
+﻿# Advent of Code 2023
+## Day 9 - Mirage Maintenance
+
+### Part 1
+#### Initial thoughts
+asd 
+asd
+asd
+
+### Implementation
+asd
+asd
+asd
+
+#### Solution : XXXX
+---
+### Part 2
+#### Initial thoughts
+asd 
+asd
+asd
+
+### Implementation
+asd
+asd
+asd
+
+#### Solution : XXXX
