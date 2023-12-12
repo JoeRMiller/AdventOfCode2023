395e357cafa7033eb97601f659e5b5672f535150
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-12 02:19:19 -0800

    Added Generic Tree and TreeNode data structures, with  pre and post order traversal to both Tree and Nodes, to Core utilities project.

--- Core/Utility.cs
+++ Core/Utility.cs
@@ -1,10 +1,136 @@
 ﻿namespace AdventofCode2023.Core
 {
+
+    public class Tree<T>
+    {
+        public TreeNode<T> Root { get; private set; }
+
+        public Tree(T rootValue)
+        {
+            this.Root = new TreeNode<T>(rootValue); 
+        }
+
+        public void PreFirstTraversal(TreeNode<T> node, Action<T> visit)
+        {
+            if (node == null) return;
+
+            visit(node.Value);
+
+            foreach (var child in node.Nodes)
+            {
+                PreFirstTraversal(child, visit);
+            }
+        }
+
+        public void PreFirstTraversal(TreeNode<T> node, Func<T, T> visit)
+        {
+            if (node == null) return;
+
+            visit(node.Value);
+
+            foreach (var child in node.Nodes)
+            {
+                PreFirstTraversal(child, visit);
+            }
+        }
+
+        public void PostOrderTraversal(TreeNode<T> node, Action<T> visit)
+        {
+            if (node == null) return;
+
+            foreach (var child in node.Nodes)
+            {
+                PostOrderTraversal(child, visit);
+            }
+
+            visit(node.Value);
+        }
+
+        public void PostOrderTraversal(TreeNode<T> node, Func<T, T> visit)
+        {
+            if (node == null) return;
+
+            foreach (var child in node.Nodes)
+            {
+                PostOrderTraversal(node, visit);
+            }
+
+            visit(node.Value);
+        }
+    }
+
+    public class TreeNode<T>
+    {
+        public T Value { get; set; }
+        public List<TreeNode<T>> Nodes { get; private set; }
+
+        public TreeNode(T value)
+        {
+            this.Value = value;
+            this.Nodes = [];
+        }
+
+        public void AddNode(TreeNode<T> node)
+        {
+            this.Nodes.Add(node);
+        }
+
+        public void PreFirstTraversal(TreeNode<T> node, Action<T> visit)
+        {
+            if (node == null) return;
+
+            visit(node.Value);
+
+            foreach (var child in node.Nodes)
+            {
+                PreFirstTraversal(child, visit);
+            }
+        }
+
+        public void PreFirstTraversal(TreeNode<T> node, Func<T, T> visit)
+        {
+            if (node == null) return;
+
+            visit(node.Value);
+
+            foreach (var child in node.Nodes)
+            {
+                PreFirstTraversal(child, visit);
+            }
+        }
+
+        public void PostOrderTraversal(TreeNode<T> node, Action<T> visit)
+        {
+            if (node == null) return;
+
+            foreach (var child in node.Nodes)
+            {
+                PostOrderTraversal(child, visit);
+            }
+
+            visit(node.Value);
+        }
+
+        public void PostOrderTraversal(TreeNode<T> node, Func<T, T> visit)
+        {
+            if (node == null) return;
+
+            foreach (var child in node.Nodes)
+            {
+                PostOrderTraversal(node, visit);
+            }
+
+            visit(node.Value);
+        }
+    }
+
+        
     public class Utility
     {
         public static List<string> ReadProjectFile(string path)
         {
             return File.ReadAllLines($"..\\..\\..\\{path}").ToList();
         }
+
     }
 }
