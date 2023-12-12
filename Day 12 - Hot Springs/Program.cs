ec5fb81ffa1706eb6f51023a5f5883d37975313d
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-12 00:23:11 -0800

    Created Day 12 Part 1 Project

---
+++ Day 12 - Hot Springs/Program.cs
@@ -0,0 +1,29 @@
+﻿using AdventofCode2023.Core;
+using AdventOfCode2023.Day12;
+using System.Diagnostics;
+
+Console.WriteLine("Advent of Code Day 12 - Hot Springs");
+
+//Part 1
+var watch = Stopwatch.StartNew();
+var input = Utility.ReadProjectFile("input.txt");
+//var input = Utility.ReadProjectFile("sample.txt");
+var result = 0;
+
+
+
+
+watch.Stop();
+Console.WriteLine($"Answer: {result}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
+
+
+//Part 2
+watch.Start();
+result = 0;
+
+
+
+watch.Stop();
+Console.WriteLine($"Answer: {result}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
---
+++ Day 12 - Hot Springs/Readme.md
@@ -0,0 +1,21 @@
+﻿# Advent of Code 2023
+## Day 12 - Hot Springs
+
+### Part 1
+#### Initial thoughts
+
+
+### Implementation
+
+
+#### Solution :
+---
+### Part 2
+#### Initial thoughts
+
+
+### Implementation
+
+
+
+#### Solution :
