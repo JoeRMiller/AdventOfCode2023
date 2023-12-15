218f27a2bbb3895355ed0f6be1d31ecc02c4c2af
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-14 21:25:14 -0800

    Completed Part 1

---
+++ Day 15/Helper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Day15
+{
+    public class Hash
+    {
+        public string Word { get; set; }
+        public int Value { get; private set; }
+
+        public Hash(string word)
+        {
+            this.Word = word;
+            CalculateValue();
+        }
+
+        private void CalculateValue()
+        {
+            int result = 0;
+            byte[] bytes= Encoding.ASCII.GetBytes(this.Word);
+            foreach (var chr in bytes) 
+            { 
+                int value = result + (int)chr;
+                value = value * 17;
+                value = value % 256;
+                result = value;
+            }
+            this.Value = result;
+        }
+    }
+    public static class Helper
+    {
+        public static List<Hash> GetHashList(string input) 
+        {
+            List<Hash> list = [];
+
+            var splits = input.Split(',');
+            foreach (var split in splits) 
+            {
+                Hash hash = new Hash(split);
+                list.Add(hash);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Day 15/Readme.md
@@ -0,0 +1,29 @@
+﻿# Advent of Code 2023
+## Day 15 - Lens Library
+
+### Part 1
+#### Initial thoughts
+Unless I'm reading it wrong, this seems very straightforward.
+
+	Split the input line by ,
+	for each item, calculate the hash value
+		get ascii code
+		multiply by 17
+		mod that by 256,
+		add result to previous total
+	add all the hashes up
+
+### Implementation
+Exactly as planned
+
+#### Solution : 506869
+---
+### Part 2
+#### Initial thoughts
+
+
+### Implementation
+
+
+
+#### Solution :
