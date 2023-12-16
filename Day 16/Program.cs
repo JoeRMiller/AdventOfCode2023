66c154121c164f334c29dc3aa35db49e6211d5e2
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-15 20:59:48 -0800

    Created Day 16 Project

---
+++ Day 16/Program.cs
@@ -0,0 +1,29 @@
+﻿using AdventofCode2023.Core;
+using AdventOfCode2023.Day16;
+using System.Diagnostics;
+
+Console.WriteLine("Advent of Code Day 16 - CHALLENGENAME");
+
+//Part 1
+var watch = Stopwatch.StartNew();
+var input = Utility.ReadProjectFile("input.txt");
+//var input = Utility.ReadProjectFile("sample.txt");
+var result = 0;
+
+
+
+
+watch.Stop();
+Console.WriteLine($"Answer: {result}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}\n");
+
+
+//Part 2
+watch.Start();
+result = 0;
+
+
+
+watch.Stop();
+Console.WriteLine($"Answer: {result}");
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}");
---
+++ Day 16/Readme.md
@@ -0,0 +1,21 @@
+﻿# Advent of Code 2023
+## Day 16 - CHALLENGENAME
+
+### Part 1
+#### Initial thoughts
+
+
+### Implementation
+
+
+#### Solution :
+---
+### Part 2
+#### Initial thoughts
+
+
+### Implementation
+
+
+
+#### Solution :
